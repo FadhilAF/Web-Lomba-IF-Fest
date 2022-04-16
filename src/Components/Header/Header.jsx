7cc1675f2be19eb7bf2ed0e726c1f722a382c9e8
Azie Melza Pratama <aziemp66@gmail.com>
2022-04-16 22:38:02 +0700

    Refactoring Code

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
 
 const Header = () => {
 	return (
-		<div>
+		<nav>
+			<div>
+				<span>e</span>Papua
+			</div>
 			<div>
 				<Link to="/">Home</Link>
+				<Link to="/folklore">Folk Lore</Link>
 			</div>
-			<div>
-				<Link to="/about">About</Link>
-			</div>
-		</div>
+			<div></div>
+		</nav>
 	);
 };
 
---
+++ src/Pages/FolkLore/FolkLore.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FolkLore = () => {
+	return <div>Folk Lore</div>;
+};
+
+export default FolkLore;
