740222a601b6277c91a8716e9c3a51b525a15a90
Azie Melza Pratama <aziemp66@gmail.com>
2022-04-16 23:22:14 +0700

    Completed Header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,17 +2,25 @@ import React from "react";
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
 
+import unesco from "../../Assets/unesco.png";
+
 const Header = () => {
 	return (
-		<nav>
-			<div>
+		<nav className={styles.navbar}>
+			<div className={styles.logo}>
 				<span>e</span>Papua
 			</div>
-			<div>
-				<Link to="/">Home</Link>
-				<Link to="/folklore">Folk Lore</Link>
+			<div className={styles["nav-links"]}>
+				<div>
+					<Link to="/">Home</Link>
+				</div>
+				<div>
+					<Link to="/folklore">Folk Lore</Link>
+				</div>
 			</div>
-			<div></div>
+			<div className={styles.unesco}>
+				<img src={unesco} alt="unesco-logo" />
+			</div>
 		</nav>
 	);
 };
