874b65d8d7e50f6ab2d84ae06f34c07b6c890b87
Azie Melza Pratama <aziemp66@gmail.com>
2022-04-17 16:13:36 +0700

    Completed Landing Page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import styles from "./Home.module.css";
 
 const Home = () => {
-	return <div className={styles.container}></div>;
+	return (
+		<div className={styles.container}>
+			<div className={styles.title}>
+				<h1>Papua Barat</h1>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
