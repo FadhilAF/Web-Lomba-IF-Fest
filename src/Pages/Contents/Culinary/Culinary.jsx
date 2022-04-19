5ca78ebad2d8dc01dca4f58efef9e77d8fb59c0b
FadhilAF <falfatih3421@gmail.com>
2022-04-19 20:52:56 +0700

    add Culinary Page content

--- src/Pages/Contents/Culinary/Culinary.jsx
+++ src/Pages/Contents/Culinary/Culinary.jsx
@@ -11,7 +11,31 @@ const Culinary = () => {
 					</div>
 					<div className={contentsStyles.heroContent}>
 						<p>
-							Brrr
+						This typical Papuan food is the staple food of the lowland and 
+						coastal communities of Papua. The tribes that make papeda as 
+						their staple food are the Asmat, Mappi, and Mimika tribes. 
+						Papeda itself is more popular in the Manokwari indigenous 
+						community.
+						<br /><br />
+						Papeda is a food that is rich in fiber, cholesterol, and enough 
+						to meet human nutrition. The main ingredient of papeda comes from 
+						sago flour sourced from the sago tree in Papua. Sago is already 
+						widely known by the people of eastern Indonesia.
+						<br /><br />
+						Not only that, but sago is also widely known by the Malay community 
+						such as Malaysia and Brunei. This food called papeda is also well 
+						known by the Malay community as ‘Linut’. Especially in South 
+						Sulawesi, this food is known as ‘Kapurung’.
+						<br /><br />
+						In addition to sago flour, the ingredients needed to make this dish 
+						are water and a little salt to add a salty taste. How to cook and 
+						make it is also fairly easy to include all the ingredients at once 
+						and cook until thickened.
+						<br /><br />
+						For companion ingredients to eat papeda, you can combine several 
+						side dishes such as various fish dishes, or other dishes. You can 
+						combine it with soupy food or stir-fried food which is also 
+						suitable to accompany eating papeda.
 						</p>
 					</div>
 				</div>
@@ -19,11 +43,44 @@ const Culinary = () => {
 			<section className={`${contentsStyles.content} ${culinaryStyles.caterpillar}`}>
 				<div className={contentsStyles.hero}>
 					<div className={contentsStyles.heroTitle}>
-						<h2>Sagoo Caterpillar</h2>
+						<h2>Sago Caterpillar</h2>
 					</div>
 					<div className={contentsStyles.heroContent}>
 						<p>
-							Brr
+						West Papua always offers a bunch of uniqueness, beauty, and of 
+						course exotic things which make lots of people are curious about 
+						this wonderful place. Are you one of them who are also interested 
+						in and curious about West Papua? Having a visit to enjoy the fun 
+						and beauty of this place will be such a good thing. There are so 
+						many ideas of the interesting thing people can enjoy there. Most 
+						of them are the unique things and of course we can enjoy lots of 
+						things there. That is also including the culinary. West Papua has 
+						a bunch of great unique typical foods which people can enjoy. 
+						They may sound weird, unique, and a bit strange. However, if you 
+						have a chance to visit this place, why don’t you have them a try? 
+						It is an essential thing you need to remember if you are going 
+						to this place. One of the unique typical foods in West Papua is 
+						made from the Sago Caterpillar. Sure, it is about the caterpillars 
+						which are cooked. That is actually the source of protein as like 
+						meat, chicken, pork, fishes, and many others. However, perhaps 
+						for many people especially who are from outside Papua, it is a 
+						bit strange to eat caterpillar. For the people who are in Papua, 
+						it is really common to eat the caterpillar of sago. One of the 
+						typical foods of West Papua to enjoy this kind of caterpillar is 
+						by creating the caterpillar as satay.
+						<br /><br />
+						Sago caterpillar is a nutritious thing which is high protein and 
+						gives the good impact of the body. It has the salty yet sweet 
+						taste which is hard outside but tender inside. The sago caterpillar 
+						contains high of methionin, lysine, tyrosin, glutamic acid, 
+						aspartic acid, essential amino acid, and many more. This is such 
+						a nutritious food for the people. That becomes the source of 
+						protein as well. Thus, you can fulfil the body need of the 
+						protein and other substances or nutrients by consuming this 
+						food. In addition, the sago caterpillar can be cooked in various 
+						ways. This typical food of West Papua is not only delicious but 
+						also totally nutritious. That will be a good food you must try if 
+						you are visiting West Papua and enjoying the fun time there.
 						</p>
 					</div>
 				</div>
